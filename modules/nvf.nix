bfbea51ac7236f1d30ea1ecc451ec64256869cec
jancstarcan <jan.cadezsmo@gmail.com>
2025-07-26 15:52:31 +0200

    nvf Config

--- modules/nvf.nix
+++ modules/nvf.nix
@@ -1,43 +1,46 @@
 { pkgs, ... }:
 
 {
-        home.packages = with pkgs; [
-                vscode-langservers-extracted
-        ];
-        programs.nvf = {
-                enable = true;
-                settings = {
-                        vim = {
-                                options = {
-                                        tabstop = 4;
-                                        shiftwidth = 4;
-                                        cursorline = true;
-                                };
-                                theme = {
-                                        enable = true;
-                                        name = "gruvbox";
-                                        style = "dark";
-                                        transparent = true;
-                                };
+	home.packages = with pkgs; [
+		vscode-langservers-extracted
+	];
+	programs.nvf = {
+		enable = true;
+		settings = {
+			vim = {
+				options = {
+					tabstop = 4;
+					shiftwidth = 4;
+					smartindent = true;
+					expandtab = false;
+					cursorline = true;
+					clipboard = "unnamedplus";
+				};
+				theme = {
+					enable = true;
+					name = "gruvbox";
+					style = "dark";
+					transparent = true;
+				};
 
-                                statusline.lualine.enable = true;
-                                telescope.enable = true;
-                                autocomplete.nvim-cmp.enable = true;
+				statusline.lualine.enable = true;
+				telescope.enable = true;
+				autocomplete.nvim-cmp.enable = true;
 
-                                lsp = {
-                                        enable = true;
-                                        servers = {
-                                                cssls = {};
-                                        };
-                                };
-                                languages = {
-                                        enableTreesitter = true;
+				lsp = {
+					enable = true;
+					servers = {
+						cssls = {};
+					};
+				};
+				languages = {
+					enableTreesitter = true;
 
-                                        nix.enable = true;
-                                        ts.enable = true;
-                                        rust.enable = true;
-                                };
-                        };
-                };
-        };
+					nix.enable = true;
+					ts.enable = true;
+					rust.enable = true;
+				};
+			};
+		};
+	};
 }
