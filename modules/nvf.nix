874769b0c2d65c982c21107baceadd9cd115beed
jancstarcan <jan.cadezsmo@gmail.com>
2025-07-26 13:36:17 +0200

    nvf Config

--- modules/nvf.nix
+++ modules/nvf.nix
@@ -1,34 +1,29 @@
 { ... }:
 
 {
-        programs = {
-                neovim = {
-                        enable = true;
-                        nvf.enable = true;
-                };
-                nvf = {
-                        enable = true;
-                        settings = {
-                                vim = {
-                                        theme = {
-                                                enable = true;
-                                                name = "gruvbox";
-                                                style = "dark";
-                                        };
+        programs.nvf = {
+                enable = true;
+                settings = {
+                        vim = {
+                                theme = {
+                                        enable = true;
+                                        name = "gruvbox";
+                                        style = "dark";
+                                };
 
-                                        statusline.lualine.enable = true;
-                                        telescope.enable = true;
-                                        autocomplete.nvim-cmp.enable = true;
+                                statusline.lualine.enable = true;
+                                telescope.enable = true;
+                                autocomplete.nvim-cmp.enable = true;
 
-                                        languages = {
-                                                enableLSP = true;
-                                                enableTreesitter = true;
+                                lsp.enable = true;
+                                languages = {
+                                        enableTreesitter = true;
 
-                                                nix.enable = true;
-                                                ts.enable = true;
-                                                rust.enable = true;
-                                        };
+                                        nix.enable = true;
+                                        ts.enable = true;
+                                        rust.enable = true;
                                 };
                         };
                 };
-        }
+        };
+}
