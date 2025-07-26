56d9273b902247ce4a71e75d6587a54059ff5a9b
jancstarcan <jan.cadezsmo@gmail.com>
2025-07-26 13:18:26 +0200

    nvf Config

--- modules/nvf.nix
+++ modules/nvf.nix
@@ -1,7 +1,14 @@
 { ... }:
 
 {
-	programs.nvf = {
-		enable = true;
-	};
+        programs.nvf = {
+                enable = true;
+                settings = {
+                        vim.viAlias = false;
+                        vim.vimAlias = true;
+                        vim.lsp = {
+                                enable = true;
+                        };
+                };
+        };
 }
