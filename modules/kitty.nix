6c6c11a75a17b0f736dad167e8541d2f57527374
jancstarcan <jan.cadezsmo@gmail.com>
2025-07-27 02:40:01 +0200

    Code cleanup

--- modules/kitty.nix
+++ modules/kitty.nix
@@ -1,7 +1,11 @@
 { pkgs, ... }: 
 {
-	home = {
-		packages = with pkgs; [ kitty ];
-		file.".config/kitty/kitty.conf".source = ../config/kitty/kitty.conf;
-	};
+		home.packages = with pkgs; [ kitty ];
+
+		programs.kitty = {
+			enable = true;
+			font.name = "Jetbrains Mono NerdFont";
+			font.size = 14;
+			extraConfig = builtins.readFile ../config/kitty/kitty.conf;
+		};
 }
