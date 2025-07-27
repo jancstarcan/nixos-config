6194a1539b212c5d095d40796ddb2c05ee88360d
jancstarcan <jan.cadezsmo@gmail.com>
2025-07-27 15:42:47 +0200

    Gradle install

--- home.nix
+++ home.nix
@@ -1,62 +1,63 @@
 { pkgs, ... }:
 
 let
-myAliases = {
-	nrsf = "sudo nixos-rebuild switch --flake /home/jan/.dotfiles/";
-	hmsf = "home-manager switch --flake /home/jan/.dotfiles/";
-	se = "sudoedit";
-	lg = "lazygit";
-	".." = "cd ..";
-};
+	myAliases = {
+		nrsf = "sudo nixos-rebuild switch --flake /home/jan/.dotfiles/";
+		hmsf = "home-manager switch --flake /home/jan/.dotfiles/";
+		se = "sudoedit";
+		lg = "lazygit";
+		".." = "cd ..";
+	};
 in
 
-{
+	{
 	imports = [
-	./modules/import-modules.nix
+		./modules/import-modules.nix
 	];
 	home.username = "jan";
 	home.homeDirectory = "/home/jan";
 
 	home.stateVersion = "25.05";
 
-        home.packages = with pkgs; [
-                hello
-                capitaine-cursors
-                wl-clipboard
-                swww
-                librewolf
-                libgcc
-                gcc
-                yazi
-                legcord
-                qbittorrent
-                ntfs3g
-                bitwarden
-                qutebrowser
-                lazygit
-                prismlauncher
+	home.packages = with pkgs; [
+		hello
+		capitaine-cursors
+		wl-clipboard
+		swww
+		librewolf
+		libgcc
+		gcc
+		yazi
+		legcord
+		qbittorrent
+		ntfs3g
+		bitwarden
+		qutebrowser
+		lazygit
+		prismlauncher
+		gradle
 	];
 
-		home = {
-			sessionVariables = {
-				EDITOR = "nvim";
-				SUDO_EDITOR = "nvim";
-			};
-			pointerCursor = {
-				name = "capitaine-cursors";
-				package = pkgs.capitaine-cursors;
-				size = 24;
-			};
+	home = {
+		sessionVariables = {
+			EDITOR = "nvim";
+			SUDO_EDITOR = "nvim";
 		};
+		pointerCursor = {
+			name = "capitaine-cursors";
+			package = pkgs.capitaine-cursors;
+			size = 24;
+		};
+	};
 
-		gtk = {
-			enable = true;
-			cursorTheme = {
-				name = "capitaine-cursors";
-				package = pkgs.capitaine-cursors;
-				size = 24;
-			};
+	gtk = {
+		enable = true;
+		cursorTheme = {
+			name = "capitaine-cursors";
+			package = pkgs.capitaine-cursors;
+			size = 24;
 		};
+	};
 
 	systemd.user.startServices = true;
 
@@ -77,8 +78,8 @@ in
 				init.defaultBranch = "main";
 			};
 		};
-};
+	};
 
-# Let Home Manager install and manage itself.
-programs.home-manager.enable = true;
+	# Let Home Manager install and manage itself.
+	programs.home-manager.enable = true;
 }
