1d336dad93c5fa1a4346a9a0a8eeddd0d0958e11
jancstarcan <jan.cadezsmo@gmail.com>
2025-07-28 22:55:20 +0200

    xremap

--- modules/xremap.nix
+++ modules/xremap.nix
@@ -6,7 +6,7 @@
 			extraArgs = ["--watch"];
 
 			config = {
-				keymap = [
+				/* keymap = [
 				{
 					name = "Caplock to Esc";
 					remap = {
@@ -15,7 +15,31 @@
 						Grave = "CapsLock";
 					};
 				}
-				];
-			};
+				]; */
+			modmap = [
+				{
+					name = "CapsLock to Esc/RControl";
+					remap = {
+						"Capslock" = {
+							held = "Ctrl_R";
+							alone = "Esc";
+							alone_timeout = 150;
+						};
+						"Esc" = {
+							held = "Grave";
+							alone = "Grave";
+							alone_timeout = 1;
+							timeout = 1;
+						};
+						"Grave" = {
+							held = "CapsLock";
+							alone = "CapsLock";
+							alone_timeout = 1;
+							timeout = 1;
+						};
+					};
+				}
+			];
 		};
-	}
+	};
+}
