ac9443bc51631cbffc4df8585b1f336008cdc5fc
jancstarcan <jan.cadezsmo@gmail.com>
2025-07-28 22:36:27 +0200

    xremap

--- modules/xremap.nix
+++ modules/xremap.nix
@@ -8,34 +8,11 @@
 			config = {
 				keymap = [
 				{
-					name = "Caps and hjkl -> arrows";
+					name = "Caplock to Esc";
 					remap = {
-						Ctrl_R-h = "Left";
-						Ctrl_R-j = "Down";
-						Ctrl_R-k = "Up";
-						Ctrl_R-l = "Right";
-					};
-				}
-				];
-				modmap = [
-				{
-					name = "CapsLock to Esc/RControl";
-					remap = {
-						CapsLock = {
-							held = "Ctrl_R";
-							alone = "Esc";
-							alone_timeout = 150;
-						};
-						Esc = {
-							held = "Grave";
-							alone = "Grave";
-							alone_timeout = 0;
-						};
-						Grave = {
-							held = "CapsLock";
-							alone = "CapsLock";
-							alone_timeout = 0;
-						};
+						CapsLock = "Esc";
+						Esc = "Grave";
+						Grave = "CapsLock";
 					};
 				}
 				];
