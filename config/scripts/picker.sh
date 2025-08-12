9cf981199811a7a3292725d54126b35318c8082b
jancstarcan <jan.cadezsmo@gmail.com>
2025-08-13 01:31:15 +0200

    Color picker script

--- config/scripts/picker.sh
+++ config/scripts/picker.sh
@@ -1,2 +1,2 @@
 #!/home/jan/.nix-profile/bin/zsh
-bindsym $mod+Shift+p exec grim -g "$(slurp -p)" -t ppm - | convert - -format '%[pixel:p{0,0}]' txt:- | tail -n 1 | cut -d ' ' -f 4 | wl-copy
+grim -g "$(slurp -p)" -t ppm - | convert - -format '%[pixel:p{0,0}]' txt:- | tail -n 1 | cut -d ' ' -f 4 | wl-copy
